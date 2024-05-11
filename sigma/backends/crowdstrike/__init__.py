45dca1bbf16b62a368726dc519c0982c9135d430
Panos Moullotos <25383165+moullos@users.noreply.github.com>
2024-05-11 17:55:31 +0300

    renaming a file

--- sigma/backends/crowdstrike/__init__.py
+++ sigma/backends/crowdstrike/__init__.py
@@ -1,4 +1,4 @@
-from .crowdstrike import LogScaleBackend
+from .logscale import LogScaleBackend
 
 backends = {        # Mapping between backend identifiers and classes. This is used by the pySigma plugin system to recognize backends and expose them with the identifier.
     "CrowdStrike Logscale": LogScaleBackend,
